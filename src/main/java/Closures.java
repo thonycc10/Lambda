9015b9283eaf4e903201ee6fc9268fe6b343e11f
Anthony Carrasco <thony10.rc.c@gmail.com>
2023-06-28 14:03:22 -0500

    closures

---
+++ src/main/java/Closures.java
@@ -0,0 +1,16 @@
+import Interface.Task;
+
+public class Closures {
+    public static void main(String[] args) {
+        int val = 111;
+        Task lambda = () -> {
+            System.out.println(val);
+            System.out.println("Task Completed");
+        };
+        printValue(lambda);
+    }
+
+    private static void printValue(Task lambda) {
+        lambda.doTask();
+    }
+}
---
+++ src/main/java/Interface/Task.java
@@ -0,0 +1,6 @@
+package Interface;
+
+@FunctionalInterface
+public interface Task {
+    void doTask();
+}
